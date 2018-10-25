d091ef9e4c21b852a0222de05ab944d779ed54fd
Johnny Zarate <jklzforyt@gmail.com>
2018-10-25 11:47:38 +0800

    raw files

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// sample
+/**
+ * @description
+ * Shoepatos
+ */
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  req.viewModel = {
+    title: 'Shoepatos'
+  };
+  next();
+});
+
+app.set('views', path.join(__dirname, 'server/views'));
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index.pug');
+  })
+
+//app.use('/', indexRouter);
+
+app.listen(port, (err) => {
+  if(err) { return console.error(err); }
+  console.log(`Listening to ${port}...`);
+});
